0358ff4d9e14674803ccc028231a6713a12fcb63
nadun <you@example.com>
2025-11-21 20:05:53 +0530

    start to update server js

---
+++ server/server.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const path = require('path')
+
+const Message = require('./models/Message')
